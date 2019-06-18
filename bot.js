16d2d4c7c819eca13422d85aedac7bc1bfcdb3c0
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-06-18 15:38:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,19 +20,6 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-    let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('$bcbot')){
-    if(!message.author.id === '') return;
-    message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
-    client.users.forEach(m =>{
-    m.sendMessage(args)
-    })
-    }
-    });
-
-
-
 
 
 
